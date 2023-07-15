1ca46b5443710840b7c31a16347d0314378d2bce
Masood Ahmadi <masoodahmadi553@gmail.com>
2023-07-15 21:54:55 +0300

    homepage updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 
 import embeddedFlag from './pngegg.png';
+import useWindowSize from './use-window-dimentions.hook';
 function App() {
+  const size = useWindowSize();
+
   return (
     <>
       {/* <Navbars /> */}
@@ -12,103 +15,206 @@ function App() {
               <Card.Title className='d-flex justify-content-center'>
                 <h3 style={{ textTransform: 'uppercase' }}>Afghanistan</h3>
               </Card.Title>
+              {size.width <= 390 ? (
+                <>
+                  <Card
+                    style={{
+                      border: 'none',
+                      boxShadow:
+                        'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;',
+                    }}
+                  >
+                    <Row>
+                      <Col style={{ backgroundColor: '#fff' }}>
+                        <div className='d-flex align-items-center flex-column gap-4'>
+                          <Card
+                            style={{
+                              backgroundColor: 'red',
+                              height: '10rem',
+                              width: '100%',
 
-              <Card
-                style={{
-                  border: 'none',
-                  boxShadow:
-                    'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;',
-                }}
-              >
-                <Row>
-                  <Col style={{ backgroundColor: '#fff' }}>
-                    <div className='d-flex align-items-center flex-column gap-4'>
-                      <Card
-                        style={{
-                          backgroundColor: 'red',
-                          height: '50rem',
-                          width: '100%',
+                              boxShadow: 'rgba(255, 40, 0, 1) 0px 15px 25px',
+                            }}
+                          >
+                            <span style={{ color: '#fff' }}></span>
+                          </Card>
+                          <Button
+                            variant='primary'
+                            style={{
+                              width: '100%',
+                              backgroundColor: 'red',
+                              border: 'none',
+                            }}
+                          >
+                            Cities
+                          </Button>
+                        </div>
+                      </Col>
+                      <Col style={{ backgroundColor: '#fff' }}>
+                        <div className='d-flex align-items-center flex-column gap-4'>
+                          <Card
+                            style={{
+                              backgroundColor: 'black',
+                              height: '10rem',
+                              width: '100%',
 
-                          boxShadow: 'rgba(255, 40, 0, 1) 0px 15px 25px',
-                        }}
-                      >
-                        <span style={{ color: '#fff' }}></span>
-                      </Card>
-                      <Button
-                        variant='primary'
-                        style={{
-                          width: '100%',
-                          backgroundColor: 'red',
-                          border: 'none',
-                        }}
-                      >
-                        Cities
-                      </Button>
-                    </div>
-                  </Col>
-                  <Col style={{ backgroundColor: '#fff' }}>
-                    <div className='d-flex align-items-center flex-column gap-4'>
-                      <Card
-                        style={{
-                          backgroundColor: 'black',
-                          height: '50rem',
-                          width: '100%',
+                              boxShadow: 'rgba(0, 0, 0, 1) 0px 15px 25px',
+                            }}
+                          >
+                            <span style={{ color: '#fff' }}>
+                              <div className=''>
+                                <div className='position-absolute top-50 start-50 translate-middle'>
+                                  <img
+                                    className=''
+                                    alt='embedded-flag'
+                                    src={embeddedFlag}
+                                    height={size.width <= 390 ? 90 : 300}
+                                  />
+                                </div>
+                              </div>
+                            </span>
+                          </Card>
+                          <Button
+                            variant='primary'
+                            style={{
+                              width: '100%',
+                              backgroundColor: 'black',
+                              border: 'none',
+                            }}
+                          >
+                            Cities
+                          </Button>
+                        </div>
+                      </Col>
+                      <Col style={{ backgroundColor: '#fff' }}>
+                        <div className='d-flex align-items-center flex-column gap-4'>
+                          <Card
+                            style={{
+                              backgroundColor: 'green',
+                              height: '10rem',
+                              width: '100%',
+                              boxShadow: 'rgba(48, 155, 35, 1) 0px 15px 25px',
+                            }}
+                          >
+                            <span style={{ color: '#fff' }}></span>
+                          </Card>
 
-                          boxShadow: 'rgba(0, 0, 0, 1) 0px 15px 25px',
-                        }}
-                      >
-                        <span style={{ color: '#fff' }}>
-                          <div className=''>
-                            <div class='position-absolute top-50 start-50 translate-middle'>
-                              <img
-                                className=''
-                                alt='embedded-flag'
-                                src={embeddedFlag}
-                                height={300}
-                              />
-                            </div>
-                          </div>
-                        </span>
-                      </Card>
-                      <Button
-                        variant='primary'
-                        style={{
-                          width: '100%',
-                          backgroundColor: 'black',
-                          border: 'none',
-                        }}
-                      >
-                        Cities
-                      </Button>
-                    </div>
-                  </Col>
-                  <Col style={{ backgroundColor: '#fff' }}>
-                    <div className='d-flex align-items-center flex-column gap-4'>
-                      <Card
-                        style={{
-                          backgroundColor: 'green',
-                          height: '50rem',
-                          width: '100%',
-                          boxShadow: 'rgba(48, 155, 35, 1) 0px 15px 25px',
-                        }}
-                      >
-                        <span style={{ color: '#fff' }}></span>
-                      </Card>
+                          <Button
+                            variant='primary'
+                            style={{
+                              width: '100%',
+                              backgroundColor: 'green',
+                              border: 'none',
+                            }}
+                          >
+                            Cities
+                          </Button>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card>
+                </>
+              ) : (
+                <>
+                  {' '}
+                  <Card
+                    style={{
+                      border: 'none',
+                      boxShadow:
+                        'rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;',
+                    }}
+                  >
+                    <Row>
+                      <Col style={{ backgroundColor: '#fff' }}>
+                        <div className='d-flex align-items-center flex-column gap-4'>
+                          <Card
+                            style={{
+                              backgroundColor: 'red',
+                              height: '50rem',
+                              width: '100%',
 
-                      <Button
-                        variant='primary'
-                        style={{
-                          width: '100%',
-                          backgroundColor: 'green',
-                          border: 'none',
-                        }}
-                      >
-                        Cities
-                      </Button>
-                    </div>
-                  </Col>
-                </Row>
-              </Card>
+                              boxShadow: 'rgba(255, 40, 0, 1) 0px 15px 25px',
+                            }}
+                          >
+                            <span style={{ color: '#fff' }}></span>
+                          </Card>
+                          <Button
+                            variant='primary'
+                            style={{
+                              width: '100%',
+                              backgroundColor: 'red',
+                              border: 'none',
+                            }}
+                          >
+                            Cities
+                          </Button>
+                        </div>
+                      </Col>
+                      <Col style={{ backgroundColor: '#fff' }}>
+                        <div className='d-flex align-items-center flex-column gap-4'>
+                          <Card
+                            style={{
+                              backgroundColor: 'black',
+                              height: '50rem',
+                              width: '100%',
+
+                              boxShadow: 'rgba(0, 0, 0, 1) 0px 15px 25px',
+                            }}
+                          >
+                            <span style={{ color: '#fff' }}>
+                              <div className=''>
+                                <div className='position-absolute top-50 start-50 translate-middle'>
+                                  <img
+                                    className=''
+                                    alt='embedded-flag'
+                                    src={embeddedFlag}
+                                    height={size.width <= 364 ? 90 : 300}
+                                  />
+                                </div>
+                              </div>
+                            </span>
+                          </Card>
+                          <Button
+                            variant='primary'
+                            style={{
+                              width: '100%',
+                              backgroundColor: 'black',
+                              border: 'none',
+                            }}
+                          >
+                            Cities
+                          </Button>
+                        </div>
+                      </Col>
+                      <Col style={{ backgroundColor: '#fff' }}>
+                        <div className='d-flex align-items-center flex-column gap-4'>
+                          <Card
+                            style={{
+                              backgroundColor: 'green',
+                              height: '50rem',
+                              width: '100%',
+                              boxShadow: 'rgba(48, 155, 35, 1) 0px 15px 25px',
+                            }}
+                          >
+                            <span style={{ color: '#fff' }}></span>
+                          </Card>
+
+                          <Button
+                            variant='primary'
+                            style={{
+                              width: '100%',
+                              backgroundColor: 'green',
+                              border: 'none',
+                            }}
+                          >
+                            Cities
+                          </Button>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Card>
+                </>
+              )}
             </Card.Body>
           </Card>
         </div>
--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='pt-5'>
+      <div className='pt-5'>
+        <App />
+      </div>
+    </div>
   </React.StrictMode>
 );
 

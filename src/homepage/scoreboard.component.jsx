b09a893b4245eb768d4b8d64712a8306e9c77504
Masood Ahmadi <masoodahmadi553@gmail.com>
2023-07-16 23:00:36 +0300

    table added

--- src/homepage/scoreboard.component.jsx
+++ src/homepage/scoreboard.component.jsx
@@ -10,10 +10,19 @@ import { PlusSquare } from 'react-bootstrap-icons';
 import { TeamModal } from './modal';
 // import generate from './screenshot';
 
-const initialValue = {
-  playername: '',
-  teamname: '',
-};
+const highScores = [
+  { id: '1', name: '', score: 0 },
+  { id: '2', name: '', score: 0 },
+  { id: '3', name: '', score: 0 },
+  { id: '4', name: '', score: 0 },
+  { id: '5', name: '', score: 0 },
+  { id: '6', name: '', score: 0 },
+  { id: '7', name: '', score: 0 },
+  { id: '8', name: '', score: 0 },
+  { id: '9', name: '', score: 0 },
+  { id: '10', name: '', score: 0 },
+  { id: '10', name: '', score: 0 },
+];
 
 function ScoreBoard() {
   const history = useHistory();
@@ -21,8 +30,10 @@ function ScoreBoard() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const [show, setShow] = useState(false);
-  const [items, setItems] = useState(initialValue);
+  const [items, setItems] = useState(highScores);
   const [name, setName] = useState(items.teamname);
+  const [chalk, setChalk] = useState('');
+
   const [palyname, setPlayerName] = useState('');
   const [notes, setNotes] = useState(() => {
     const savedState = localStorage.getItem('stateString');
@@ -33,30 +44,45 @@ function ScoreBoard() {
   useEffect(() => {
     localStorage.setItem('stateString', JSON.stringify(notes));
   }, [notes]);
-  useEffect(() => {
-    const items = JSON.parse(localStorage.getItem('stateString'));
-    if (items) {
-      setItems(items);
-    }
-  }, []);
 
-  useEffect(() => {
-    const items = JSON.parse(localStorage.getItem('stateString'));
-    if (items) {
-      setItems(items);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const items = JSON.parse(localStorage.getItem('stateString'));
+  //   if (items) {
+  //     setItems(items);
+  //   }
+  // }, []);
   const updateNotes = (event) => {
     event.preventDefault();
-    var newNotes = notes.slice();
-    newNotes.push(palyname);
-    setPlayerName('');
-    setNotes(newNotes);
+    // var newNotes = notes.slice();
+    if (palyname === '') {
+      return;
+    } else {
+      setNotes(palyname);
+      // newNotes.push(palyname);
+    }
   };
 
-  const notesList = notes.map((note) => note);
-  console.log('notesList', notesList);
-  // console.log('name', name);
+  const updateChalk = (event) => {
+    setChalk(event.target.value);
+  };
+  const updateNotes2 = (event) => {
+    event.preventDefault();
+    var newNotes = notes.slice();
+    newNotes.push(chalk);
+    setChalk('');
+    setNotes(newNotes);
+  };
+  const notesList = notes.map((note) => (
+    <li
+      style={{
+        borderBottom: '2px solid black',
+        padding: ' 5px 0 0',
+      }}
+    >
+      {note}
+    </li>
+  ));
+  // console.log('notesList', notesList);
   return (
     <div>
       <Container>
@@ -92,6 +118,53 @@ function ScoreBoard() {
                     aria-describedby='basic-addon1'
                   />
                 </InputGroup>
+
+                <form onSubmit={updateNotes2}>
+                  <input
+                    type='text'
+                    placeholder='type here'
+                    value={chalk}
+                    onChange={updateChalk}
+                  />
+
+                  <input type='submit' />
+                </form>
+
+                <div
+                  className='board'
+                  style={{
+                    backgroundColor: 'darkgreen',
+                    height: '150px',
+                    borderBottom: ' 20px solid brown',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginTop: ' 20px',
+                    borderRadius: ' 10px 10px 0px 0px',
+                  }}
+                >
+                  <h1
+                    className='chalk'
+                    style={{
+                      color: 'white',
+                      opacity: '0.9',
+                    }}
+                  >
+                    {chalk}
+                  </h1>
+                </div>
+
+                <ul
+                  className='notes'
+                  style={{
+                    textAlign: 'left',
+                    border: ' 1px solid gray',
+                    margin: '10px 50px',
+                    padding: '50px 20px 10px 30px',
+                  }}
+                >
+                  {notesList}
+                </ul>
               </Card.Text>
               <Button variant='primary' onClick={updateNotes}>
                 Add player
@@ -121,7 +194,7 @@ function ScoreBoard() {
             </thead>
             <tbody>
               <tr className='text-center text-bold'>
-                <th scope='row'>{notesList}</th>
+                {/* <th scope='row'>{notesList}</th> */}
                 <th scope='row'>0</th>
                 <th scope='row'>0</th>
                 <th scope='row'>0</th>
